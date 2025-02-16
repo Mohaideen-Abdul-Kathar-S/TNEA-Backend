8e021fdd030514be2cbb6cdd33659e3711db9579
Mohaideen Abdul Kathar S <157392970+Mohaideen-Abdul-Kathar-S@users.noreply.github.com>
2025-02-16 11:23:31 +0530

    Delete java/com/example/tut directory

--- java/com/example/tut/repository/MessageSysRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tut.repository;
-
-import com.example.tut.model.MessageSys;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MessageSysRepository extends MongoRepository<MessageSys, String> {
-    MessageSys findByMsgId(String msgId);
-}
